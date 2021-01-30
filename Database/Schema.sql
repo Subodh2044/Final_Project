0d27efdb75e2c0775d21cecf76131b5dc17a91e3
Reno Stephens <reno8946@yahoo.com>
2021-01-30 03:54:18 -0600

    Adding updated ERD Schema colab file

---
+++ Database/Schema.sql
@@ -0,0 +1,49 @@
+CREATE TABLE "Airline" (
+
+    "Gender" varchar   NOT NULL,
+
+    "Customer" varchar   NOT NULL,
+
+    "Age" int   NOT NULL,
+
+    "Type of Travel" varchar   NOT NULL,
+
+    "Class" varchar   NOT NULL,
+
+    "Flight Distance" int   NOT NULL,
+
+    "Inflight wifi" int   NOT NULL,
+
+    "Departure/Arrival time convenient" int   NOT NULL,
+	
+	"Ease of Online booking" int   NOT NULL,
+	
+	"Gate location" int   NOT NULL,
+	
+	"Food and drink" int   NOT NULL,
+	
+	"Online boarding" int   NOT NULL,
+	
+	"Seat comfort" int   NOT NULL,
+	
+	"Inflight entertainment" int   NOT NULL,
+	
+	"On-board service" int   NOT NULL,
+	
+	"Leg room service" int   NOT NULL,
+	
+	"Baggage handling" int   NOT NULL,
+	
+	"Checkin service" int   NOT NULL,
+	
+	"Inflight service" int   NOT NULL,
+	
+	"Cleanliness" int   NOT NULL,
+	
+	"Departure Delay in Minutes" int   NOT NULL,
+	
+	"Arrival Delay in Minutes" int   NOT NULL,
+	
+	"satisfaction" varchar   NOT NULL
+
+);
