3878c9e436a716d0dea9c340664066d5a55dc5d7
Reno Stephens <reno8946@yahoo.com>
2021-01-31 16:43:50 -0600

    Add files via upload
    Adding final files for database role

--- Database/Schema.sql
+++ Database/Schema.sql
@@ -1,49 +1,50 @@
-CREATE TABLE "Airline" (
+Create TABLE airline (
+	 
+	"Gender" varchar NOT NULL,
 
-    "Gender" varchar   NOT NULL,
+    "Customer" varchar NOT NULL,
 
-    "Customer" varchar   NOT NULL,
+    "Age" int NOT NULL,
 
-    "Age" int   NOT NULL,
+    "Type_of_Travel" varchar NOT NULL,
 
-    "Type of Travel" varchar   NOT NULL,
+    "Class" varchar NOT NULL,
 
-    "Class" varchar   NOT NULL,
+    "Flight Distance" int NOT NULL,
 
-    "Flight Distance" int   NOT NULL,
+    "Inflight wifi" int NOT NULL,
 
-    "Inflight wifi" int   NOT NULL,
-
-    "Departure/Arrival time convenient" int   NOT NULL,
+    "Departure/Arrival_time_convenient" int NOT NULL,
 	
-	"Ease of Online booking" int   NOT NULL,
+	"Ease_of_Online booking" int NOT NULL,
 	
-	"Gate location" int   NOT NULL,
+	"Gate_location" int NOT NULL,
 	
-	"Food and drink" int   NOT NULL,
+	"Food_and_drink" int NOT NULL,
 	
-	"Online boarding" int   NOT NULL,
+	"Online boarding" int NOT NULL,
 	
-	"Seat comfort" int   NOT NULL,
+	"Seat comfort" int NOT NULL,
 	
-	"Inflight entertainment" int   NOT NULL,
+	"Inflight entertainment" int NOT NULL,
 	
-	"On-board service" int   NOT NULL,
+	"On-board service" int NOT NULL,
 	
-	"Leg room service" int   NOT NULL,
+	"Leg room service" int NOT NULL,
 	
-	"Baggage handling" int   NOT NULL,
+	"Baggage handling" int NOT NULL,
 	
-	"Checkin service" int   NOT NULL,
+	"Checkin service" int NOT NULL,
 	
-	"Inflight service" int   NOT NULL,
+	"Inflight service" int NOT NULL,
 	
-	"Cleanliness" int   NOT NULL,
+	"Cleanliness" int NOT NULL,
 	
-	"Departure Delay in Minutes" int   NOT NULL,
+	"Departure_Delay_in_Minutes" int NOT NULL,
 	
-	"Arrival Delay in Minutes" int   NOT NULL,
+	"Arrival_Delay_in_Minutes" numeric NOT NULL,
 	
-	"satisfaction" varchar   NOT NULL
+	"satisfaction" varchar NOT NULL 
 
 );
+
