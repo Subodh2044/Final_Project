0868d0eca1c32999df369d6f59d388b647c1f63c
Reno Stephens <reno8946@yahoo.com>
2021-02-03 02:48:23 -0600

    Adding updated Spark file and Schema and ERD

---
+++ Database/Database_Schema.sql
@@ -0,0 +1,99 @@
+Create TABLE train (
+	 
+	"Gender" varchar NOT NULL,
+
+    "Customer" varchar NOT NULL,
+
+    "Age" numeric NOT NULL,
+
+    "Type_of_Travel" varchar NOT NULL,
+
+    "Class" varchar NOT NULL,
+
+    "Flight Distance" numeric NOT NULL,
+
+    "Inflight wifi" numeric NOT NULL,
+
+    "Departure/Arrival_time_convenient" numeric NOT NULL,
+	
+	"Ease_of_Online booking" numeric NOT NULL,
+	
+	"Gate_location" numeric NOT NULL,
+	
+	"Food_and_drink" numeric NOT NULL,
+	
+	"Online boarding" numeric NOT NULL,
+	
+	"Seat comfort" numeric NOT NULL,
+	
+	"Inflight entertainment" numeric NOT NULL,
+	
+	"On-board service" numeric NOT NULL,
+	
+	"Leg room service" numeric NOT NULL,
+	
+	"Baggage handling" numeric NOT NULL,
+	
+	"Checkin service" numeric NOT NULL,
+	
+	"Inflight service" numeric NOT NULL,
+	
+	"Cleanliness" numeric NOT NULL,
+	
+	"Departure_Delay_in_Minutes" numeric NOT NULL,
+	
+	"Arrival_Delay_in_Minutes" numeric NOT NULL,
+	
+	"satisfaction" varchar NOT NULL 
+
+);
+
+Create TABLE test (
+	 
+	"Gender" varchar NOT NULL,
+
+    "Customer" varchar NOT NULL,
+
+    "Age" numeric NOT NULL,
+
+    "Type_of_Travel" varchar NOT NULL,
+
+    "Class" varchar NOT NULL,
+
+    "Flight Distance" numeric NOT NULL,
+
+    "Inflight wifi" numeric NOT NULL,
+
+    "Departure/Arrival_time_convenient" numeric NOT NULL,
+	
+	"Ease_of_Online booking" numeric NOT NULL,
+	
+	"Gate_location" numeric NOT NULL,
+	
+	"Food_and_drink" numeric NOT NULL,
+	
+	"Online boarding" numeric NOT NULL,
+	
+	"Seat comfort" numeric NOT NULL,
+	
+	"Inflight entertainment" numeric NOT NULL,
+	
+	"On-board service" numeric NOT NULL,
+	
+	"Leg room service" numeric NOT NULL,
+	
+	"Baggage handling" numeric NOT NULL,
+	
+	"Checkin service" numeric NOT NULL,
+	
+	"Inflight service" numeric NOT NULL,
+	
+	"Cleanliness" numeric NOT NULL,
+	
+	"Departure_Delay_in_Minutes" numeric NOT NULL,
+	
+	"Arrival_Delay_in_Minutes" numeric NOT NULL,
+	
+	"satisfaction" varchar NOT NULL 
+
+);
